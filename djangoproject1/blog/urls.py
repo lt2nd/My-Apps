80381abe97fb53a7a256212bf29f02462bdbc7fb
lt2nd <miloradovicivan@gmail.com>
2021-03-31 20:59:54 +0200

    Django-blog
    Simple blog app

---
+++ djangoproject1/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+#    path('', views.home, name='Blog-Home'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='user-posts'),
+    path('about/', views.about, name='Blog-About'),
+    path('',views.PostListView.as_view(), name="Blog-Home"),
+    path('post/<int:pk>/',views.PostDetailView.as_view(), name="post-detail"),
+    path('post/new/',views.PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/',views.PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/',views.PostDeleteView.as_view(), name="post-delete"),
+]
---
+++ djangoproject1/djangoproject1/urls.py
@@ -0,0 +1,47 @@
+"""djangoproject1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/',
+    views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/',
+    views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+    name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+    views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+    name='password_reset_confirm'),
+    path('', include('blog.urls')),
+    path('', include('users.urls')),
+    path('password-reset-complete/',
+    views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+    name='password_reset_complete'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#    urlpatterns = [
+        # the rest of settings goes HERE
+#    ] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
